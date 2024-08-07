ad9c3497bc71128c2615a365f2fa3001397b7cf6
outsider-kithy <outsider.design.tech@gmail.com>
2024-08-07 13:46:33 +0900

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { View, Text, Image } from 'react-native';
+import * as tf from '@tensorflow/tfjs';
+import { fetch, decodeJpeg } from '@tensorflow/tfjs-react-native';
+import * as mobilenet from '@tensorflow-models/mobilenet';
 
-export default function App() {
+const App = () => {
+  const [isTfReady, setIsTfReady] = useState(false);
+  const [result, setResult] = useState('');
+  const image = useRef(null);
+
+  const load = async () => {
+    try {
+      // Load mobilenet.
+      await tf.ready();
+      const model = await mobilenet.load();
+      setIsTfReady(true);
+
+      // Start inference and show result.
+      const image = require('./assets/myImage.jpg');
+      const imageAssetPath = Image.resolveAssetSource(image);
+      const response = await fetch(imageAssetPath.uri, {}, { isBinary: true });
+      const imageDataArrayBuffer = await response.arrayBuffer();
+      const imageData = new Uint8Array(imageDataArrayBuffer);
+      const imageTensor = decodeJpeg(imageData);
+      const prediction = await model.classify(imageTensor);
+      if (prediction && prediction.length > 0) {
+        setResult(
+          `${prediction[0].className} (${prediction[0].probability.toFixed(3)})`
+        );
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    load();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Image
+        ref={image}
+        source={require('./assets/myImage.jpg')}
+        style={{ width: 200, height: 200 }}
+      />
+      {!isTfReady && <Text>Loading TFJS model...</Text>}
+      {isTfReady && result === '' && <Text>Classifying...</Text>}
+      {result !== '' && <Text>{result}</Text>}
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ README.md
@@ -0,0 +1,45 @@
+# React Native(Expo) + TensorFlow.jsで物体検出アプリ
+MobileNetを利用して、画像から物体を検出するスマホアプリ
+
+## バージョン
+
+"@tensorflow-models/mobilenet": "^2.1.1",
+"@tensorflow/tfjs": "^4.20.0",
+"@tensorflow/tfjs-react-native": "^1.0.0",
+"expo": "~51.0.24",
+"expo-status-bar": "~1.12.1",
+"react": "18.2.0",
+"react-native": "0.74.3"
+
+## 注意点(2024年8月時点)
+
+`tfjs-react-native`のCameraコンポーネントには最新の`react-native`との間に互換性がないので、`@tensorflow/tfjs-react-native/dist/index.js`内でコメントアウトする必要あり。
+
+```JavaScript:@tensorflow/tfjs-react-native/dist/index.js
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+import './platform_react_native';
+export { asyncStorageIO } from './async_storage_io';
+export { bundleResourceIO } from './bundle_resource_io';
+export { decodeJpeg } from './decode_image';
+export { fetch } from './platform_react_native';
+export { version } from './version';
+// 以下をコメントアウト！
+// export * from './camera/camera';
+// export * from './camera/camera_stream';
+//# sourceMappingURL=index.js.map
+```
